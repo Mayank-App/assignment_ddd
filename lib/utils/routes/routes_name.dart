228919268ad55f4ef0c8890694591660e13a3f21
Mayank Dubey <mayank.dubey@appinventiv.com>
2024-02-01 17:16:11 +0530

    api response reflected and convert Routes to auto routes

--- lib/utils/routes/routes_name.dart
+++
@@ -1,6 +0,0 @@
-class RoutesName {
-  static const login = "Login_Page";
-  static const singup = "Sign_In";
-  static const splashScreen = "Splash_Screen";
-  static const homeScreen  = "Home_Screen";
-}
